a2f0a32f1d60589a5b2e5e0352d3a0283dd0a689
shu-gh <hirosawa14175@gmail.com>
2021-02-28 11:05:33 +0900

    test1

---
+++ polls/admin.py
@@ -0,0 +1,13 @@
+
+
+# Register your models here.
+from django.contrib import admin
+
+from .models import Question
+
+admin.site.register(Question)
+
+
+from .models import Choice
+
+admin.site.register(Choice)
